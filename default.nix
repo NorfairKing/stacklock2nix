dc42acce50d9904c5c803ab49ca5a90a79cc0d87
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-23 18:13:50 +0200

    something that doesnt work for _different_ reasons

--- default.nix
+++ default.nix
@@ -6,6 +6,7 @@
 
 let
   lib = pkgs.lib;
+
   traceId = x: builtins.trace x x;
   fromYamlFile = pkgs.callPackage ./lib/fromYamlFile.nix { };
 
@@ -89,6 +90,42 @@ let
   # The contents of the stack.yaml file in nix format
   stackYamlContents = fromYamlFile stackYaml;
 
+  hiddenPackagesSet =
+    builtins.mapAttrs (_: value: null) snapshotFileContents.hidden // {
+      array = null;
+      base = null;
+      binary = null;
+      bytestring = null;
+      Cabal = null;
+      containers = null;
+      deepseq = null;
+      directory = null;
+      filepath = null;
+      ghc-boot = null;
+      ghc-boot-th = null;
+      ghc-compact = null;
+      ghc-heap = null;
+      ghc-prim = null;
+      ghci = null;
+      haskeline = null;
+      hpc = null;
+      integer-gmp = null;
+      libiserv = null;
+      mtl = null;
+      parsec = null;
+      pretty = null;
+      process = null;
+      rts = null;
+      stm = null;
+      template-haskell = null;
+      terminfo = null;
+      text = null;
+      time = null;
+      transformers = null;
+      unix = null;
+      xhtml = null;
+    };
+
 
   localPackagePath = builtins.head stackYamlContents.packages;
 
@@ -101,7 +138,9 @@ let
       { };
 
   totalPackageSetFunction = args@{ pkgs, lib, callPackage }: self:
-    snapshotPackageSetFunction args self // ({ "local-test" = localPkgDerivationFunc self args; });
+    hiddenPackagesSet //
+    snapshotPackageSetFunction args self //
+    ({ "local-test" = localPkgDerivationFunc self args; });
 
 
   haskellPackagesFunc = pkgs.haskell.lib.makePackageSet {
@@ -120,4 +159,4 @@ let
 
 
 in
-haskellPackages.validity
+haskellPackages.local-test
